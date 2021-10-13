0c446f70c0ebce23d9d3ff754bb18a5049469da8
danilobenatti <danilobenatti@gmail.com>
2021-10-13 11:22:29 -0300

    Incluído método para consumir um POST da API livraria-virtual. PARTE 2

--- src/main/java/br/com/uniciv/rest/MainPost.java
+++ src/main/java/br/com/uniciv/rest/MainPost.java
@@ -3,20 +3,28 @@ package br.com.uniciv.rest;
 import javax.ws.rs.client.ClientBuilder;
 import javax.ws.rs.client.Entity;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.core.Response.Status;
 
 public class MainPost {
 
 	public static void main(String[] args) {
 
-		Livro livro = new Livro(3L, "Livro C", "ISBN-9999", "Gênero A", 19.99,
+		Livro livro = new Livro(4L, "Livro D", "ISBN-0000", "Gênero A", 19.99,
 				"Autor 1");
 
-		Livro livroResposta = ClientBuilder.newClient()
+		Response response = ClientBuilder.newClient()
 				.target("http://localhost:8080/livraria-virtual").path("livro")
 				.request()
-				.post(Entity.entity(livro, MediaType.APPLICATION_XML),
-						Livro.class);
-		System.out.println(livroResposta.getTitulo());
+				.post(Entity.xml(livro));
+		if (response.getStatus() == Status.CREATED.getStatusCode()) {
+			ItemBusca itemBusca = ClientBuilder.newClient()
+					.target(response.getLocation())
+					.request(MediaType.APPLICATION_XML).get(ItemBusca.class);
+			System.out.println(itemBusca.getLivro().getTitulo());
+		} else if (response.getStatus() == Status.BAD_REQUEST.getStatusCode()) {
+			System.out.println("Verifique se todos os dados estão corretos");
+		}
 
 	}
 
